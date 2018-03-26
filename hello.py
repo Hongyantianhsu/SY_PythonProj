6409eed453761f9a280259148b3f84935de3d8c9
货拉拉-孙岩 <sunyan.sun@huolala.cn>
2018-03-26 17:14:45 +0800

    python学习01-基础语法

--- hello.py
+++ hello.py
@@ -1,3 +1,39 @@
-#!/usr/bin/env python
 #coding=utf-8
-print('你好')
+'''
+这是我的第一个python代码
+so exciting!
+'''
+"""
+这是另一种注释方式
+"""
+
+if True:
+	print '你好'
+else:
+	print '傻逼'
+
+days = ["Monday" , "Tuesday" , "Wednesday"];
+print days
+
+# 等待用户输入
+raw_input("请按下 enter 键退出，其他任意键显示...\n");
+
+# 不换行输出
+a = "sunyan";
+b = "hello";
+print a,
+print b
+# 换行输出
+print a
+print b
+# 不换行输出
+print a,b
+
+#多个语句构成代码组
+if a == "sunyan":
+	print "11111"
+elif a == "hello":
+	print "2222"
+else:
+	pass
+
