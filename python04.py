81f2232f193a9bd02947f3f098052e3db5434a84
货拉拉-孙岩 <sunyan.sun@huolala.cn>
2018-03-28 14:22:26 +0800

    推倒99乘法表时间复杂度

--- python04.py
+++ python04.py
@@ -6,12 +6,14 @@ for x in range(1,10):
 for x in 'python':
 	print x
 
-#=====================================>
+print "=====================================>"
+
 #99乘法表
-for i in range(1,10):
-	for j in range(1,i+1):
-		if i*j < 10:
-			print i,"*",j,"=","",i*j,
+for i in range(1,10):   
+	for j in range(1,i+1):  
+		if i*j < 10: 
+			print i,"*",j,"=","", #O(1)
 		else :
-			print i,"*",j,"=",i*j,
-	print
+			print i,"*",j,"=",i*j, #O(1)
+	print #O(1)
+#T(n) = O(f(n)) = (1+n)n/2 + 1 + 1 + 1 = O(n^2) = O(81)
